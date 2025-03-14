a95a4fabfad8d295ed2c81c4d650ae0d70b8c2cd
eyad alzhrany <ayadalzhrany640@gmail.com>
2025-03-14 17:51:17 +0300

    Update the-script.lua

--- the-script.lua
+++ the-script.lua
@@ -1,5 +1,8 @@
+--tp saver
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xsi1c/TP-saver/refs/heads/main/TP-Saver.lua"))()
-
+--old tp saver 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xsi1c/TP-saver/refs/heads/main/old-tp-saver.lua"))()
 
 --i tested it in lua so you can 
 --tell me if it didnt work with u
+--discord: xsi1c
